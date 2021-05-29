4121bef6591377d4174a89ca695019132cb42f71
Camille Dejoye <camilledejoye@gmail.com>
2021-05-29 14:01:40 +0200

    reorganize some of the scripts

--- 20-bspwm.sh
+++ 20-bspwm.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
 
-# Variables
+# Variables {{{
+
 repository="https://github.com/camilledejoye/dotfiles"
 
-# Install dependencies to build yay
+# }}}
+
+# Install an AUR helper {{{
+
+## Install dependencies to build yay
 sudo pacman -S --noconfirm --needed base-devel
 
-# Build yay & cleanup
-cd /tmp; git clone https://aur.archlinux.org/yay.git
-cd yay; makepkg --cleanbuild --syncdeps --install --noconfirm
-cd /tmp; rm -fr yay
+## Build yay & cleanup
+git clone https://aur.archlinux.org/yay.git /tmp/yay
+cd /tmp/yay
+makepkg --cleanbuild --syncdeps --install --noconfirm
+cd /tmp
+rm -fr yay
 
-# Install packages
+# }}}
+
+# Install packages {{{
+
+## General packages {{{
+
 yay -S --noconfirm --needed \
   xorg \
   lightdm \
@@ -23,47 +35,73 @@ yay -S --noconfirm --needed \
   bspwm \
   sxhkd \
   polybar \
-  ttf-font-awesome \
-  ttf-nerd-fonts-symbols-mono \
-  ttf-icomoon-feather \
   dunst \
   vlc \
   alacritty \
-  ttf-hack \
   rofi \
   rofi-dmenu \
   feh \
-  lxappearance \
   picom \
   arandr \
   autorandr \
   scrot \
-  arc-gtk-theme \
-  arc-icon-theme \
-  gnome-themes-extra \
-  gtk-engine-murrine \
   rcm-git \
   xclip \
   brightnessctl \
-  ranger w3m imagemagick\
+  ranger w3m \
   jq \
   udiskie \
   dex \
+  pcmanfm \
   qutebrowser
 
-# Install separately because it requires dmenu and this will conflict with rofi-dmenu
-# This way rofi-dmenu will validate the requirements without conflicts
-# Since --noconfirm will fail if there is a conflict it stops the rest of the script otherwise
+## Install separately because it requires dmenu and this will conflict with rofi-dmenu
+## This way rofi-dmenu will validate the requirements without conflicts
+## Since --noconfirm will fail if there is a conflict it stops the rest of the script otherwise
 yay -S --noconfirm --needed clipmenu
 
-# Enable lightdm
-sudo systemctl enable lightdm
+# }}}
 
-# Setup X11 keyboard layout (needed for lightdm)
+## Theme related packages {{{
+
+yay -S --noconfirm --needed \
+  lxappearance \
+  arc-gtk-theme \
+  arc-icon-theme \
+  sardi-icons \
+  gtk-engine-murrine \
+  bibata-cursor-theme-bin
+
+# }}}
+
+## Fonts {{{
+yay -S --noconfirm --needed \
+  ttf-dejavu \
+  ttf-droid \
+  ttf-hack \
+  ttf-font-awesome \
+  ttf-ms-fonts \
+  ttf-roboto \
+  ttf-ubuntu-font-family \
+  noto-fonts \
+  terminus-font
+
+# }}}
+
+# }}}
+
+# Configuration {{{
+
+## Install dictionaries for qutebrowser
+/usr/share/qutebrowser/scripts/dictcli.py install fr-FR en-US
+
+## Setup X11 keyboard layout (needed for lightdm)
 localectl --no-convert set-x11-keymap fr oss terminate:ctrl_alt_bksp,grp:ctrl_rshift_toggle,caps:escape
 
-# Clone the dotfiles repository
-# Pull only the submodules I'll need
+## Dotfiles {{{
+
+### Clone the dotfiles repository
+### Pull only the submodules I'll need
 git clone "$repository" \
   -b rcm \
   --recurse-submodules=oh-my-zsh \
@@ -73,16 +111,23 @@ git clone "$repository" \
   --recurse-submodules=config/nvim/pack/packager/opt/vim-packager \
   .dotfiles
 
-# Deploy the dotfiles
-
+### Deploy the dotfiles
 echo "Deploying the dotfiles..."
 # Force the deployement since it's a fresh install
 # Don't deploy the vim configuration right now because the plugins require acces to my github
 rcup -f -x config/nvim/
 echo "Dotfiles deployed."
 
-# Enable user services from the dotfiles
+## }}}
+
+## Enable services {{{
+
+sudo systemctl enable lightdm
 systemctl --user enabe ssh-agent.service
+
+## }}}
+
+# }}}
 
 echo -e "\e[1;34m"
 echo "The second part of the installation is over"
@@ -92,4 +137,4 @@ echo "  and run the last installation script"
 echo
 echo "$ systemctl reboot"
 
-# vim: ts=2 sw=2 et
+# vim: ts=2 sw=2 et fdm=marker
